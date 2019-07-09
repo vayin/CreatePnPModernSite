c2027a2296c9505956b816c361c218f6e7c6c46a
vinay ayinapurapu <deep.ayina@gmail.com>
2019-07-09 18:43:41 -0500

    Add files via upload
    Adding the raw version

---
+++ CreatingModernSiteFrmCSV.ps1
@@ -0,0 +1,53 @@
+﻿#Write in professional manner
+#Regions, variables, functions, Try catch, logging,
+Import-Module Microsoft.Online.SharePoint.Powershell -ErrorAction SilentlyContinue
+
+Add-PSSnapIn Microsoft.SharePoint.PowerShell  -ErrorAction SilentlyContinue
+
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+Add-Type -Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.WorkflowServices.dll"
+
+ 
+# Authenticate with the SharePoint site. 
+
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+$ScriptDirCSV = $ScriptDir+"\SiteURLS.csv"
+
+$SiteURLS = Import-Csv $ScriptDirCSV
+
+
+<#$username = Read-Host -Prompt "Enter admin email:" 
+$password = Read-Host -Prompt "Enter admin password:" -AsSecureString
+$adminUrl = Read-Host -Prompt "Enter the Tenant Admin URL" 
+#Connect-PnPOnline -Url $adminUrl -Credentials($username, $password);
+#Other method
+[System.Management.Automation.PSCredentials]$PSCredentials=New-Object System.Management.Automation.PSCredential($username,$password)#>
+$AdminUrl=Read-Host -Prompt "Input the Site URL to connect via PNP"
+Connect-PnPOnline -Url $AdminUrl -UseWebLogin
+
+ForEach($item in $SiteURLS)
+{
+ $Title=$item.("Title");
+ $SiteUrl=$item.("SiteURL");
+ $Template=$item.("Template");
+ $Description=$item.("Description");
+ $owner=$item.("Owner");
+
+New-PnPTenantSite `
+  -Title $Title `
+  -Url $SiteUrl `
+  -Description $Description `
+  -Owner $owner `
+  -Lcid 1033 `
+  -Template $Template `
+  -TimeZone 11 `
+  -StorageQuota 51200 `
+  -StorageQuotaWarningLevel 45000 `
+  -Wait
+
+
+Write-Host("Site Provision Completed $Title") -BackgroundColor Green; 
+}
+
+
---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+Disclaimer: 
+
+Please note that this code is provided as is. No warranties or support is provided. Always run first in our Dev Tenant to validate the results and proceed to production.
+
+Note:  
+You need to install latest PNP powershell commandlets inorder to work. for installing the powershell pnp commands please refer to the below URL
+https://docs.microsoft.com/en-us/powershell/sharepoint/sharepoint-pnp/sharepoint-pnp-cmdlets?view=sharepoint-ps
